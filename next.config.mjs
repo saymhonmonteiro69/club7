8ad7847fbc18108e3f846ccfc55036ae8ae935b4
saymhonmonteiro69 <saymhon.monteiro69@gmail.com>
2026-07-29 13:04:37 -0400

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,10 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  // ADICIONE ESTE BLOCO AQUI:
   images: {
     unoptimized: true,
   },
-  // Mantenha outras configurações que você já tiver abaixo, se houver
 };
 
-module.exports = nextConfig;
+export default nextConfig;
