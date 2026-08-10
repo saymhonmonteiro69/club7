59f0792926d8807ed9978115cfb3e63ae62433b3
saymhonmonteiro69 <saymhon.monteiro69@gmail.com>
2026-08-10 09:24:26 -0400

    Update whatsapp.ts

--- lib/whatsapp.ts
+++ lib/whatsapp.ts
@@ -1,4 +1,4 @@
-export const WHATSAPP_NUMBER = "5592986470747"
+export const WHATSAPP_NUMBER = "5592993875478"
 
 export function WHATSAPP_LINK(message: string) {
   return `https://wa.me/${WHATSAPP_NUMBER}?text=${encodeURIComponent(message)}`
