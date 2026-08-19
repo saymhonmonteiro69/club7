f9725bfdc20a1149c316123cba5abf3c270b2bc8
saymhonmonteiro69 <saymhon.monteiro69@gmail.com>
2026-08-19 14:09:46 -0400

    Update Facebook Pixel ID in layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -41,7 +41,7 @@ export default function RootLayout({
             t.src=v;s=b.getElementsByTagName(e)[0];
             s.parentNode.insertBefore(t,s)}(window, document,'script',
             'https://connect.facebook.net/en_US/fbevents.js');
-            fbq('init', '2274999913255983');
+            fbq('init', '2299072187295346');
             fbq('track', 'PageView');
           `}
         </Script>
@@ -50,7 +50,7 @@ export default function RootLayout({
             height="1"
             width="1"
             style={{ display: "none" }}
-            src="https://www.facebook.com/tr?id=2274999913255983&ev=PageView&noscript=1"
+            src="https://www.facebook.com/tr?id=2299072187295346&ev=PageView&noscript=1"
           />
         </noscript>
       </head>
